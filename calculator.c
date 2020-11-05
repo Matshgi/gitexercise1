1d13a48ef129817c3849d001475149f43c1a6cf1
Matshgi <72394778+Matshgi@users.noreply.github.com>
2020-11-05 11:27:28 +0100

    Update calculator.c

--- calculator.c
+++ calculator.c
@@ -6,7 +6,7 @@
 int main(){
 
 	float num1;
-	int num2;
+	float num2;
 	float ans;
 	char op;
 	printf("\nEnter an operator (+,-,/,*): \n");
@@ -15,15 +15,24 @@ int main(){
 	printf("\nEnter two numbers: ");
 	scanf("%f",&num1);
 	scanf("%f",&num2);
-	
+
 	switch(op){
-		case '+' : 
+		case '+' :
 			ans = num1 + num2;
 			break;
+        case '-':
+            ans = num1-num2;
+            break;
+        case '/':
+            ans=num1/num2;
+            break;
+        case '*':
+            ans=num1*num2;
+            break;
 		default:
 			printf("Enter a correct operator!");
 			return 1;
-	
+
 	}
 	printf("\n The result is as follows:\n");
 	printf("%f %c %f = %f",num1, op ,num2,ans);
